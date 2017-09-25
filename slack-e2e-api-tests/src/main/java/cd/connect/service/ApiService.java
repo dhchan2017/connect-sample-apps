fd49166659dd57cbeec417f46ddc64033150e486
Dhivya Chandran <dhivya.chandran@clearpoint.co.nz>
2017-09-25 14:36:52 +1300

    Add slack sentiment analyser api tests to extract message count from the slack channel

--- slack-e2e-api-tests/src/main/java/cd/connect/service/ApiService.java
+++ slack-e2e-api-tests/src/main/java/cd/connect/service/ApiService.java
@@ -2,14 +2,19 @@ package cd.connect.service;
 
 import cd.connect.samples.slackapp.api.MessagesService;
 import cd.connect.samples.slackapp.api.Messagelist;
+import cd.connect.samples.slackapp.api.SentimentService;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.proxy.WebResourceFactory;
 import org.glassfish.jersey.filter.LoggingFilter;
-
-
+import org.json.simple.JSONObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 public class ApiService {
 
@@ -19,6 +24,32 @@ public class ApiService {
 
 	public MessagesService messagesApi() {
 		return WebResourceFactory.newResource(MessagesService.class, webTarget);
+	}
+	public SentimentService sentimentApi() {
+		return WebResourceFactory.newResource(SentimentService.class, webTarget);
+	}
+	public Response slackApi() {
+
+		client = ClientBuilder.newClient();
+		JSONObject body = new JSONObject();
+		body.put("text", "Connect slack api\ntesting");
+		String url = "https://hooks.slack.com/services/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";
+
+		Form form = new Form();
+
+		WebTarget webTarget = client.target(UriBuilder.fromPath(url).build());
+
+		try {
+			Response response  = webTarget.request().post(Entity.entity(body, MediaType.APPLICATION_JSON));
+			System.out.println(response.readEntity(String.class));
+
+			return response;
+
+		} catch (Exception e) {
+			System.out.println("Failed to get response");
+
+			throw e;
+		}
 	}
 
 }
--- slack-e2e-api-tests/src/test/java/steps/MyStepdefs.java
+++ slack-e2e-api-tests/src/test/java/steps/MyStepdefs.java
@@ -1,17 +1,16 @@
 package steps;
 
+import cd.connect.samples.slackapp.api.Sentimentsummary;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cd.connect.samples.slackapp.api.MessagesService;
 import cd.connect.samples.slackapp.api.Messagelist;
 import cd.connect.service.ApiService;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
-
 import static org.fest.assertions.api.Assertions.assertThat;
 
 
@@ -20,8 +19,32 @@ public class MyStepdefs {
 //	private static Logger logger = LoggerFactory.getLogger(MyStepdefs.class);
 
 	private ApiService apiService;
-
 	Messagelist messagelist;
+	Response slackList;
+	Sentimentsummary sentimentSummary;
+	BigDecimal previousCount = new BigDecimal(0);
+	BigDecimal currentCountAfterNewMessage = new BigDecimal(0);
+
+	private BigDecimal getCurrentMessageCount(){
+
+		BigDecimal count = new BigDecimal(0);
+
+		sentimentSummary = apiService.sentimentApi().gETSentiment();
+
+		for (int i = 0; i < sentimentSummary.getChannels().size(); i++) {
+
+			String channelName = sentimentSummary.getChannels().get(i).getChannel();
+
+			if (channelName.contentEquals("connect-testing"))
+
+			{
+				count = sentimentSummary.getChannels().get(i).getMessageCount();
+			}
+
+		}
+		return count;
+
+	}
 
 	public MyStepdefs(ApiService apiService) {
 		this.apiService = apiService;
@@ -40,11 +63,43 @@ public class MyStepdefs {
 		assertThat(messagelist).isNotEmpty();
 	}
 
-	@Given("^a (.*) is sent through slack api$")
-	public void aMessageIsSentThroughSlackApi() throws Throwable {
+	@Given("^a message is sent to a slack channel$")
+	public void aMessageIsSentToSlackApi() throws Throwable {
 
-
-
+		slackList = apiService.slackApi();
 
 	}
+
+	@Then("^a valid response is received with (.*)$")
+	public void theResponseContainsMessage(int status) throws Throwable {
+
+		assertThat(slackList.toString()).contains("ok");
+		assertThat(slackList.getStatus()).isEqualTo(status);
+
+	}
+
+	@And("I wait for (.*) seconds")
+	public void wait(int seconds) {
+		try {
+			Thread.sleep(seconds * 2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Then("^the message count of slack sentiment analyser api should be increased to one$")
+	public void theMessageCountIncreasedToOne() throws Throwable {
+
+		currentCountAfterNewMessage = getCurrentMessageCount();
+		assertThat(currentCountAfterNewMessage).isEqualTo(previousCount.add(new BigDecimal(1)));
+
+	}
+
+	@Given("^a message count is taken before sending a message to slack channel$")
+	public void previousMessageCount() throws Throwable {
+
+		previousCount = getCurrentMessageCount();
+
+	}
+
 }
